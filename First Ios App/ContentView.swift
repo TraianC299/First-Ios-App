94ecf8b4d9cf23e425928f1c938e1bfbc249fa2d
TraianC299 <traianciochina@gmail.com>
2022-07-27 13:18:12 +0300

    Initial Commit

---
+++ First Ios App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First Ios App
+//
+//  Created by Traian on 27.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First Ios App/First_Ios_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_Ios_AppApp.swift
+//  First Ios App
+//
+//  Created by Traian on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct First_Ios_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
